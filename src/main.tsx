b9f586f94ac7fa68ab22bc1d050df690f4ce19f7
elliotyim <yimsb02@naver.com>
2025-06-03 11:20:20 +0300

    fix: Make editor work without cdn

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
+import { loader } from '@monaco-editor/react'
+import * as monaco from 'monaco-editor'
 import { createRoot } from 'react-dom/client'
 import 'react-virtualized/styles.css'
 import App from './App.tsx'
 import './index.css'
+import './userWorker.ts'
+
+loader.config({ monaco })
 
 createRoot(document.getElementById('root')!).render(
   <>
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   base: '/',
   plugins: [react(), tailwindcss()],
