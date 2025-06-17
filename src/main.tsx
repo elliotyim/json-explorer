60364953b9c7eb9d2975a7095b90629b34987130
elliotyim <yimsb02@naver.com>
2025-06-17 17:03:44 +0300

    fix: Simplify monaco editor settings

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,8 @@
-import { loader } from '@monaco-editor/react'
-import * as monaco from 'monaco-editor'
 import { createRoot } from 'react-dom/client'
 import 'react-virtualized/styles.css'
 import App from './App.tsx'
 import './index.css'
-import './userWorker.ts'
-
-loader.config({ monaco })
-loader.init()
+import './monaco.ts'
 
 createRoot(document.getElementById('root')!).render(
   <>
